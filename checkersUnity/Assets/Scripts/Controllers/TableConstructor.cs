23f8fd40ead5c9e2920641ff6534501c67300c80
Erick Guimaraes <erickgdeoliveira@gmail.com>
2020-10-11 21:29:19 -0300

    Chaging more things to fit paradigms, trying initial behaiour and setting scripts to right table

---
+++ checkersUnity/Assets/Scripts/Controllers/TableConstructor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableConstructor : MonoBehaviour
+{
+    [SerializeField]
+    private Transform centralPosition;
+    GameObject boardGameObject;
+    private Board board;
+
+    private void Awake()
+    {
+        BoardCounstructor();
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BoardCounstructor() 
+    {
+        boardGameObject = Resources.Load<GameObject>("Board");
+        Instantiate(boardGameObject, centralPosition);
+        board = boardGameObject.GetComponent<Board>();
+        var boardPieces = board.GetBoardMatrix();
+        Debug.Log(boardPieces);
+        //int totalBoardPieces = boardPieces.Length *
+    }
+}
--- checkersUnity/Assets/Scripts/Interfaces/IBoard.cs
+++ checkersUnity/Assets/Scripts/Interfaces/IBoard.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-interface IBoard 
+interface IBoard
 {
     void ConstructBoard();
     void OnChangeBehaviour();
